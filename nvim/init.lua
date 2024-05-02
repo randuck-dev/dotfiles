3de711851b901f25c10f4a9ef7c4558e6084b2dc
Raphael Neumann <mail@raphaelneumann.dk>
2024-05-02 19:31:46 +0200

    feat: Changing up some defaults and adding some plugins to improve the flow

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,15 +39,50 @@ require('lazy').setup({
 	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 	{'williamboman/mason.nvim'},
 	{'williamboman/mason-lspconfig.nvim'},
-	{'neovim/nvim-lspconfig'},
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+	{'neovim/nvim-lspconfig', config = function ()
+    -- We wait for the lsp to attach, and then we end up setting up the LSP keymaps
+    vim.api.nvim_create_autocmd('LspAttach', {
+      callback = function(event)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+      end
+    })
+	end},
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
 	-- completion engine
 	{'hrsh7th/cmp-nvim-lsp'},
 	{'hrsh7th/nvim-cmp'},
-    { "elentok/format-on-save.nvim" },
+  {'elentok/format-on-save.nvim'},
 
 	{'L3MON4D3/LuaSnip'},
+
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    dependencies = {'hrsh7th/nvim-cmp'},
+    config = function ()
+      require('nvim-autopairs').setup {}
+
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end
+  },
+
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
     {
@@ -63,18 +98,40 @@ require('lazy').setup({
     }
 })
 
+-- Telescope Begin
 local telescope = require('telescope.builtin')
 
+vim.keymap.set('n', '<leader>sh', telescope.help_tags, {})
+vim.keymap.set('n', '<leader>sg', telescope.git_files, {})
+vim.keymap.set('n', '<leader>sf', telescope.find_files, {})
+vim.keymap.set('n', '<leader>sb', telescope.buffers, {})
+vim.keymap.set('n', '<leader>sl', telescope.live_grep, {})
 
-vim.keymap.set('n', '<leader>ff', telescope.git_files, {})
-vim.keymap.set('n', '<C-p>', telescope.find_files, {})
-vim.keymap.set('n', '<C-b>', telescope.buffers, {})
-vim.keymap.set('n', '<leader>g', telescope.live_grep, {})
+-- Telesecope End
+
+-- Diagnostic Begin
+
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror message'})
+
+-- Diagnostic End
 vim.keymap.set('n', '<S-l>', ":bnext<CR>", {})
 vim.keymap.set('n', '<S-h>', ":bprevious<CR>", {})
 
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<leader>wh', ":split<CR>", {})
+vim.keymap.set('n', '<leader>wv', ":vsplit<CR>", {})
+
+
 local opts = { noremap = true, silent = true }
+
+-- Terraform Shortcuts Begin
 vim.keymap.set('n', '<leader>ti', ":!terraform -chdir=%:p:h init<CR>", opts)
+vim.keymap.set('n', '<leader>tp', ":!terraform -chdir=%:p:h plan<CR>", opts)
+
+-- Terraform Shortcuts End 
 
 -- If fzf is installed, we ensure that telescope is going to use it
 pcall(require('telescope').load_extension, 'fzf')
