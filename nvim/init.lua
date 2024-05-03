0a7f1144578285009e60141513a2ef1217ec2b3d
Raphael Neumann <2768009+randuck-dev@users.noreply.github.com>
2024-05-03 19:11:28 +0200

    feat: Use tokyo as theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -127,6 +127,23 @@ require('lazy').setup({
   },
 
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
   {
     "zbirenbaum/copilot.lua",
     event = "InsertEnter",
@@ -306,7 +323,7 @@ cmp.setup({
     }
 })
 
-vim.cmd [[ colorscheme catppuccin ]]
+vim.cmd [[ colorscheme tokyonight ]]
 
 
 require("ibl").setup()
