81f73427e8abc9001cbbec95e193ded52d1fb93d
Raphael Neumann <2768009+randuck-dev@users.noreply.github.com>
2024-06-27 21:06:43 +0200

    feat: frappe theme and disable bg in neovim

--- nvim/lua/randuck/set.lua
+++ nvim/lua/randuck/set.lua
@@ -38,3 +38,12 @@ vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+    vim.api.nvim_set_hl(0, "NonText", { bg = "none"})
+    vim.api.nvim_set_hl(0, "Normal", { ctermbg = "none"})
+    vim.api.nvim_set_hl(0, "NonText", { ctermbg = "none"})
+  end
+})
