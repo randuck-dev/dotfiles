d6b569d6ab75de49a0c4ac7fe22c982502b6b88a
Raphael Neumann <darophi@gmail.com>
2024-11-03 08:51:07 +0100

    feat: move nvim config into config folder to be auto symlinked

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -100,6 +100,26 @@ require('lazy').setup({
   },
 	'navarasu/onedark.nvim',
 	'nvim-lualine/lualine.nvim',
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        search = {
+          enabled = false
+        }
+      }
+    },
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
@@ -387,6 +407,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require('nvim_comment').setup()
 require("oil").setup({
   default_file_explorer = true,
+  view_options = {
+    show_hidden = true,
+  },
 })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
