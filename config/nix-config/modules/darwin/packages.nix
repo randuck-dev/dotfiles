01a2a26ffc02576d638901fd010ce2a6ec0cd76f
Raphael Neumann <mail@raphaelneumann.dk>
2025-01-12 11:35:40 +0100

    feat: dotnet 8 and 9 sdk installed

--- config/nix-config/modules/darwin/packages.nix
+++ config/nix-config/modules/darwin/packages.nix
@@ -18,4 +18,8 @@ shared-packages ++ [
   tokei
   go
   elixir
+  (with dotnetCorePackages; combinePackages [
+      sdk_8_0
+      sdk_9_0
+    ])
 ]
--- config/nix-config/modules/shared/packages.nix
+++ config/nix-config/modules/shared/packages.nix
@@ -52,4 +52,9 @@ with pkgs; [
 
   sesh
   zoxide
+
+  (with dotnetCorePackages; combinePackages [
+      sdk_8_0
+      sdk_9_0
+    ])
 ]
