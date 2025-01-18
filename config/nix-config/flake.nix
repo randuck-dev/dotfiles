5673c679d166324bdb91134a0d9fa9178c26f64b
Raphael Neumann <mail@raphaelneumann.dk>
2025-01-18 11:11:50 +0100

    maybe

--- config/nix-config/flake.nix
+++ config/nix-config/flake.nix
@@ -2,7 +2,7 @@
   description = "Configuration for Mac OS Machines";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     home-manager.url = "github:nix-community/home-manager";
     darwin = {
       url = "github:LnL7/nix-darwin/master";
