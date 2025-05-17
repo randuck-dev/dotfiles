75db4e8e7522da75efd0c30d1a8b39c5ad10277e
Raphael Neumann <2768009+randuck-dev@users.noreply.github.com>
2025-05-17 09:34:49 +0200

    add hyperkey mapping and sourcing of current lua file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,17 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+local set = vim.keymap.set
+
 require("config.lazy")
+
+function hyperset(mode, binding, cmd)
+  set(mode, "<F18>" .. binding, cmd)
+end
+
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+
+set("n", "<leader><leader>x", "<cmd>source %<CR>", {
+  desc = "Execute the current file",
+  callback = function()
+    local currentFile = vim.api.nvim_buf_get_name(0)
+    print("Sourced: " .. currentFile)
+  end,
+})
