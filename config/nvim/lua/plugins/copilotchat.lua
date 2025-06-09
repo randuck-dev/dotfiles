0b20eb4a55e921b5725ffa9b81f449e36886b835
Raphael Neumann <2768009+randuck-dev@users.noreply.github.com>
2025-06-09 21:45:29 +0200

    add tools for k8s

--- config/nvim/lua/plugins/copilotchat.lua
+++ config/nvim/lua/plugins/copilotchat.lua
@@ -3,10 +3,9 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     opts = {
       sticky = {
-        "You are a senior software engineer developing reliable, fast and readable software.",
         "#buffers:visible",
       },
-      model = "claude-3.7-sonnet",
+      model = "claude-sonnet-4",
     },
   },
 }
---
+++ pkgs/k8s.sh
@@ -0,0 +1,2 @@
+brew install k9s
+brew install --cask flux
